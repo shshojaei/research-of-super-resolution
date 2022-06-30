ecd4d3826d80d25d30bdcdae32f1f048dc4a5431
Shakiba Shojaei <79797720+shshojaei@users.noreply.github.com>
2022-06-30 20:38:52 +0430

    Create 02_my_mse.py

---
+++ Loss functions/02_my_mse.py
@@ -0,0 +1,12 @@
+import keras.backend as K
+
+def my_mse(y_true, y_pred):
+  
+  y_true = K.flatten(y_true)
+  y_pred = K.flatten(y_pred)
+
+  #convert Tensorflow tensor to numpy
+  y_true = K.cast( y_true, 'float32')
+  y_pred = K.cast( y_pred, 'float32')
+
+  return K.mean(K.square(y_pred - y_true), axis=-1)
