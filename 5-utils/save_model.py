8f808b15f47ba8077cb32bc4a5c6ed5c777afa1b
Shakiba Shojaei <79797720+shshojaei@users.noreply.github.com>
2022-08-05 22:39:12 +0430

    Update save_model.py

--- 5-utils/save_model.py
+++ 5-utils/save_model.py
@@ -1,1 +1,1 @@
-model.save('drive/MyDrive/Colab Notebooks/EDSR/models/model_EDSR_tensorflow_mae_50_epoch.h5')
+model.save('drive/MyDrive/Colab Notebooks/EDSR/models/model_EDSR_tensorflow_mae_50_epoch.tf')
