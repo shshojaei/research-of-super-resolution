77cd6e85c44f1ea11b9e1a6625c8d0352af6fafb
Shakiba Shojaei <79797720+shshojaei@users.noreply.github.com>
2022-08-05 22:39:47 +0430

    Update load_model.py

--- 5-utils/load_model.py
+++ 5-utils/load_model.py
@@ -1,1 +1,1 @@
-model = tf.keras.models.load_model('drive/My Drive/Colab Notebooks/EDSR/models/model.h5', custom_objects={'EDSRModel':EDSRModel, 'PSNR':PSNR })
+model = tf.keras.models.load_model('drive/My Drive/Colab Notebooks/EDSR/models/model.tf', custom_objects={'EDSRModel':EDSRModel, 'PSNR':PSNR })
