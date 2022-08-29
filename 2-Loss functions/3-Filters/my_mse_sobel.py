b5f02b57add41c7f6e593c35a9287ede6cbe5896
Shakiba Shojaei <79797720+shshojaei@users.noreply.github.com>
2022-08-29 17:35:49 +0430

    Create my_mse_sobel.py

---
+++ 2-Loss functions/3-Filters/my_mse_sobel.py
@@ -0,0 +1,19 @@
+!pip install tensorflow_io
+
+import keras.backend as K
+import tensorflow_io as tfio
+
+def my_mse_sobel(y_true, y_pred):
+
+  #convert Tensorflow tensor to numpy
+  y_true = K.cast( y_true, 'float32')
+  y_pred = K.cast( y_pred, 'float32')
+
+  #source: https://www.tensorflow.org/io/api_docs/python/tfio/experimental/filter/
+  y_true = tfio.experimental.filter.sobel(y_true) #input and output is a Tensor
+  y_pred = tfio.experimental.filter.sobel(y_pred)
+
+  y_true = K.flatten(y_true)
+  y_pred = K.flatten(y_pred)
+
+  return K.mean(K.square(y_pred - y_true), axis=-1)
