642aba4749d2fef07fb8624f6cddd69492411abb
Shakiba Shojaei <79797720+shshojaei@users.noreply.github.com>
2022-08-29 17:22:41 +0430

    Create fair.py

---
+++ 2-Loss functions/1-Pixelwise losses/fair.py
@@ -0,0 +1,16 @@
+import keras.backend as K
+
+def fair(y_true, y_pred):
+
+  y_true = K.flatten(y_true)
+  y_pred = K.flatten(y_pred)
+
+  #convert Tensorflow tensor to numpy
+  y_true = K.cast( y_true, 'float32')
+  y_pred = K.cast( y_pred, 'float32')
+
+  C=255.0
+  res = y_true - y_pred
+  yt = (C**2) * ( K.abs(res)/C -  K.log(1+  K.abs(res)/C))
+        
+  return K.mean(yt)
