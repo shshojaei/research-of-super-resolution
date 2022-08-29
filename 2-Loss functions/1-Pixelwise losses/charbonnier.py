30952e5e77f5413bf68493578e18ea57de14dd5e
Shakiba Shojaei <79797720+shshojaei@users.noreply.github.com>
2022-08-29 17:22:12 +0430

    Create charbonnier.py

---
+++ 2-Loss functions/1-Pixelwise losses/charbonnier.py
@@ -0,0 +1,16 @@
+import keras.backend as K
+
+def charbonnier(y_true, y_pred):
+
+  y_true = K.flatten(y_true)
+  y_pred = K.flatten(y_pred)
+
+  #convert Tensorflow tensor to numpy
+  y_true = K.cast( y_true, 'float32')
+  y_pred = K.cast( y_pred, 'float32')
+  
+  epsilon = 1e-3
+  res = y_true - y_pred
+  yt = K.sqrt((res**2) + (epsilon**2))
+        
+  return K.mean(yt)
