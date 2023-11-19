e76f4628a9231a0366c5e94cc55e1830aa717be8
Shakiba Shojaei <79797720+shshojaei@users.noreply.github.com>
2023-11-19 11:24:13 +0330

    Create gaussian_mae.py

---
+++ 2-Loss functions/3-Weighted Losses/gaussian_mae.py
@@ -0,0 +1,14 @@
+import tensorflow_addons as tfa
+import keras.backend as K
+
+def gaussian_mae(y_true, y_pred):
+
+  #convert Tensorflow tensor to numpy
+  y_true = K.cast( y_true, 'float32')
+  y_pred = K.cast( y_pred, 'float32')
+
+  #source: https://www.tensorflow.org/addons/api_docs/python/tfa/image/gaussian_filter2d
+  y_true = tfa.image.gaussian_filter2d(y_true, filter_shape=(11,11), sigma=1.5) #input and output is a Tensor
+  y_pred = tfa.image.gaussian_filter2d(y_pred, filter_shape=(11,11), sigma=1.5)
+
+  return K.mean(K.abs(y_pred - y_true), axis=-1)
