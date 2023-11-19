683dacddcdc98804bf38b5cd72cefdeb30ec5ad5
Shakiba Shojaei <79797720+shshojaei@users.noreply.github.com>
2023-11-19 11:06:01 +0330

    Update InceptionV3.py

--- 2-Loss functions/2-Perceptual Losses/InceptionV3.py
+++ 2-Loss functions/2-Perceptual Losses/InceptionV3.py
@@ -1,17 +1,19 @@
-InceptionV3().summary()
-#########################
-
 from tensorflow.keras.applications import InceptionV3
-from tensorflow.keras.models import Model
 import keras.backend as K
+from tensorflow.keras.models import Model
 
-#layer_names = [ mixed3, mixed6, mixed10] # 3,6,10
+#layer_names = [ mixed3, mixed6, mixed10]
 layer_name = "mixed10"
 
 inceptionV3 = InceptionV3(include_top=False, weights='imagenet', pooling='avg')
 inceptionV3.trainable = False
 output = inceptionV3.get_layer(layer_name).output
 model_inceptionV3 = Model(inceptionV3.input, output)
+
+#model_inceptionV3.summary()
+
+#for layer in model_inceptionV3.layers:
+#  print(layer.name)
 
 @tf.function
 def my_inceptionV3(y_true , y_pred):
@@ -27,8 +29,5 @@ def my_inceptionV3(y_true , y_pred):
 
   y_true = model_inceptionV3(y_true)
   y_pred = model_inceptionV3(y_pred)
-
-  y_true = K.flatten(y_true)
-  y_pred = K.flatten(y_pred)
 
   return K.mean(K.square(y_pred - y_true), axis=-1)
