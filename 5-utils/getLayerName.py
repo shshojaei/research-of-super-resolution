dc9105a0403e1f8d3565a02da640fe0482f9d16a
Shakiba Shojaei <79797720+shshojaei@users.noreply.github.com>
2023-11-19 10:16:13 +0330

    Update getLayerName.py

--- 5-utils/getLayerName.py
+++ 5-utils/getLayerName.py
@@ -2,3 +2,13 @@ inceptionResnet = InceptionResNetV2(include_top=False, weights=None, pooling='av
 
 for layer in inceptionResnet.layers:
   print(layer.name)
+
+###############################################################################
+from tensorflow.keras.applications.resnet50 import ResNet50
+import keras.backend as K
+from tensorflow.keras.models import Model
+
+resnet = ResNet50(include_top=False, weights='imagenet', pooling='avg')
+resnet.trainable = False
+layer_names = [layer.name for layer in resnet.layers]
+print(layer_names)
